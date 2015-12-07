3df5691ed094729c35f7f746e6933800a4662bd7
Annely <annely.aasalaid@gmail.com>
2015-12-07 21:06:22 +0200

    Leiab sageduse, kuid meetod Populaarseim ei tööta! MIngi null probleem

--- src/Proov/Tulemused.java
+++ src/Proov/Tulemused.java
@@ -3,49 +3,74 @@ package Proov;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
+import java.util.*;
 
 /**
  * Created by Annely on 1.12.2015.
  */
 public class Tulemused {
+
     ArrayList<Integer> lahendus = Test.tulemused;
+    TreeMap<Integer, ArrayList<Integer>> sorteeritudMap = new TreeMap<>();
+    HashMap<Integer, Integer> tulemused = new HashMap<>();
 
     Tulemused() {
             looAken();
-            leiaPopulaarseim();
+            leiaSagedus();
+            sisend(0);
+            populaarseimad();
+            System.out.println(populaarseimad());
+
     }
 
+       boolean sisend (Integer key) {
+           if (tulemused.containsKey(key)) {
+               int count = tulemused.get(key);
+               tulemused.put(key, count + 1);
 
-    private int leiaPopulaarseim() {
+               ArrayList<Integer> numbrid = sorteeritudMap.get(count);
+               numbrid.remove(key);
+               if (!sorteeritudMap.containsKey(count + 1)) {
+                   sorteeritudMap.put(count + 1, new ArrayList<>());
+                   sorteeritudMap.get(count + 1).add(key);
+               } else {
+                   tulemused.put(key, 1);
+                   if (!sorteeritudMap.containsKey(1)) {
+                       sorteeritudMap.put(1, new ArrayList<>());
+                       sorteeritudMap.get(1).add(key);
+                   }
+
+               }
+           }
+           return true;
+
+       }
+
+    ArrayList <Integer> populaarseimad() {
+        return sorteeritudMap.pollLastEntry().getValue();
+           }
+
+    public void leiaSagedus() {
+        /**abiks siin oli : http://stackoverflow.com/questions/8545590/java-find-the-most-popular-element-in-int-array
+         * millele lisasin Hashmapi
+         */
+
         Collections.sort(lahendus);
-
-        //abiks siin oli : http://stackoverflow.com/questions/8545590/java-find-the-most-popular-element-in-int-array
+        System.out.println(lahendus);
+        int loenda = 1;
         int eelmine = lahendus.get(0);
-        int populaarseim = lahendus.get(0);
-        int count = 1;
-        int kokku = 1;
-        for (int i = 0; i < lahendus.size(); i++) {
+        int i;
+        for (i = 1; i < lahendus.size(); i++) {
             if (lahendus.get(i) == eelmine) {
-                count++;
-            }
-            else {
-                if (count>kokku) {
-                    populaarseim = lahendus.get(i-1);
-                    kokku = count;
-                }
+                loenda++;
+                tulemused.put((lahendus.get(i)), loenda);
+            } else {
                 eelmine = lahendus.get(i);
-                count = 1;
+                loenda= 1;
+                tulemused.put((lahendus.get(i)), loenda);
             }
-
         }
-        return count > kokku ? lahendus.get(lahendus.size() - 1) : populaarseim;
-
+        System.out.println(tulemused);
     }
 
     private void looAken() {
@@ -54,6 +79,7 @@ public class Tulemused {
         Scene lopetamine = new Scene(pane, 400, 400);
         lopp.setScene(lopetamine);
         lopp.show();
+
     }
 
 
